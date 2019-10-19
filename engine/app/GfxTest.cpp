0043713ee5d392b729ee25f74493b9a94f31da53
Jeremie Pelletier <jeremiep@gmail.com>
2019-10-19 06:11:20 -0400

    Linux GLX, checking for GL version and extension support

--- engine/app/GfxTest.cpp
+++ engine/app/GfxTest.cpp
@@ -5,22 +5,13 @@
 #include "Box2D/Box2D.h"
 #include "imgui.h"
 
-#if PLATFORM_MACOS
-# include <unistd.h>
-#endif
-
-#if PLATFORM_POSIX
-# include <pthread.h>
-#endif
-
-#include <string>
-
 #if PLATFORM_ANDROID
 # include <GLES3/gl3.h>
 # include <GLES3/gl3ext.h>
 #elif PLATFORM_LINUX
 # include <GL/gl.h>
 # include <GL/glcorearb.h>
+# include <GL/glext.h>
 #elif PLATFORM_MACOS
 # include <OpenGL/gl3.h>
 # include <OpenGL/gl3ext.h>
@@ -36,7 +27,26 @@
 # error "Don't know how to include OpenGL for the target platform"
 #endif
 
+#if PLATFORM_MACOS
+# include <unistd.h>
+#endif
+
+#if PLATFORM_POSIX
+# include <pthread.h>
+#endif
+
+#include <string>
+
+using namespace std::literals;
+
 DECL_LOG_SOURCE(Test, Info);
+
+constexpr u16 glMakeVersion(i32 major, i32 minor) {
+  ASSERT(major > 0 && minor >= 0 && major < 10 && minor < 10);
+  return static_cast<u16>((major << 8) | minor);
+}
+
+constexpr u16 GL3_0 = glMakeVersion(3, 0);
 
 void jank_imgui_init();
 void jank_imgui_newFrame();
@@ -54,14 +64,19 @@ char const* jank_imgui_getClipboardText(void*) { return ""; }
 void jank_imgui_setClipboardText(void*, char const*) {}
 #endif
 
+
+// Dynamic Link Setup
+// -----------------------------------------------------------------------------
+
 #if PLATFORM_LINUX || PLATFORM_WINDOWS
 
-#if PLATFORM_WINDOWS
-#define GL1_2_PROCS \
+// TODO: looks like windows has GL1.1 and linux has GL1.4 we can directly link against
+# if PLATFORM_WINDOWS
+#  define GL1_2_PROCS \
   GL(TEXIMAGE3D, TexImage3D); \
   GL(TEXSUBIMAGE3D, TexSubImage3D)
 
-#define GL1_3_PROCS \
+#  define GL1_3_PROCS \
   GL(COMPRESSEDTEXIMAGE1D, CompressedTexImage1D); \
   GL(COMPRESSEDTEXIMAGE2D, CompressedTexImage2D); \
   GL(COMPRESSEDTEXIMAGE3D, CompressedTexImage3D); \
@@ -69,18 +84,21 @@ void jank_imgui_setClipboardText(void*, char const*) {}
   GL(COMPRESSEDTEXSUBIMAGE2D, CompressedTexSubImage2D); \
   GL(COMPRESSEDTEXSUBIMAGE3D, CompressedTexSubImage3D)
 
-#define GL1_4_PROCS \
+#  define GL1_4_PROCS \
   GL(BLENDEQUATION, BlendEquation)
-#endif
+# endif
 
-#define GL1_5_PROCS \
+# define GL1_5_PROCS \
   GL(BINDBUFFER, BindBuffer); \
   GL(BUFFERDATA, BufferData); \
   GL(BUFFERSUBDATA, BufferSubData); \
   GL(DELETEBUFFERS, DeleteBuffers); \
   GL(GENBUFFERS, GenBuffers)
 
-#define GL2_0_PROCS \
+# define GL2_0_ARB \
+  GL(GENFRAMEBUFFERSARB, GenFramebuffersARB)
+
+# define GL2_0_PROCS \
   GL(ATTACHSHADER, AttachShader); \
   GL(COMPILESHADER, CompileShader); \
   GL(CREATEPROGRAM, CreateProgram); \
@@ -101,7 +119,7 @@ void jank_imgui_setClipboardText(void*, char const*) {}
   GL(USEPROGRAM, UseProgram); \
   GL(VERTEXATTRIBPOINTER, VertexAttribPointer)
 
-#define GL3_0_PROCS \
+# define GL3_0_PROCS \
   GL(BINDRENDERBUFFER, BindRenderbuffer); \
   GL(BINDFRAMEBUFFER, BindFramebuffer); \
   GL(BINDVERTEXARRAY, BindVertexArray); \
@@ -116,38 +134,40 @@ void jank_imgui_setClipboardText(void*, char const*) {}
   GL(GENVERTEXARRAYS, GenVertexArrays); \
   GL(GETSTRINGI, GetStringi)
 
-#define GL3_1_PROCS \
+# define GL3_1_PROCS \
 
-#define GL3_2_PROCS \
+# define GL3_2_PROCS \
   GL(DRAWELEMENTSBASEVERTEX, DrawElementsBaseVertex)
 
-#define GL3_3_PROCS \
+# define GL3_3_PROCS \
   GL(DELETESAMPLERS, DeleteSamplers); \
   GL(GENSAMPLERS, GenSamplers)
 
 // TODO GL4
 
-#define GL(type, name) static PFNGL##type##PROC gl##name
-#if PLATFORM_WINDOWS
+# define GL(type, name) static PFNGL##type##PROC gl##name
+# if PLATFORM_WINDOWS
 GL1_2_PROCS;
 GL1_3_PROCS;
 GL1_4_PROCS;
-#endif
+# endif
 GL1_5_PROCS;
 GL2_0_PROCS;
 GL3_0_PROCS;
 GL3_1_PROCS;
 GL3_2_PROCS;
 GL3_3_PROCS;
-#undef GL
-
+# undef GL
 #endif
+
 
 // OpenGL Helpers
 // -----------------------------------------------------------------------------
 
-static void glCheck() {
-  if (auto e{ glGetError() }; e != GL_NO_ERROR) {
+#define glCheck() glCheckImpl(__FILE__, __LINE__)
+
+static void glCheckImpl(char const* file, u32 line) {
+  if (auto e{ glGetError() }; UNLIKELY(e != GL_NO_ERROR)) {
     char const* str;
     switch (e) {
 #define E(x) case GL_##x: str = #x; break
@@ -159,12 +179,12 @@ static void glCheck() {
 #undef E
     default: ASSERT(0, "Unknown GL Error: %04x", e); UNREACHABLE;
     }
-    ASSERT(0, str);
+    assertFailure(file, line, str);
   }
 }
 
 static void glCheckFramebuffer() {
-  if (auto e{ glCheckFramebufferStatus(GL_FRAMEBUFFER) }; e != GL_FRAMEBUFFER_COMPLETE) {
+  if (auto e{ glCheckFramebufferStatus(GL_FRAMEBUFFER) }; UNLIKELY(e != GL_FRAMEBUFFER_COMPLETE)) {
     char const* str;
     switch (e) {
 #define E(x) case GL_FRAMEBUFFER_##x: str = #x; break;
@@ -183,7 +203,7 @@ static void glCheckFramebuffer() {
   static void name(u32 object) { \
     i32 success; \
     get(object, status, &success); \
-    if (!success) { \
+    if UNLIKELY(!success) { \
       i32 size; \
       get(object, GL_INFO_LOG_LENGTH, &size); \
       std::string s; \
@@ -196,6 +216,320 @@ static void glCheckFramebuffer() {
 GL_CHECK_INFO_LOG(glCheckShader,  glGetShaderiv,  glGetShaderInfoLog,  GL_COMPILE_STATUS, "compile shader")
 GL_CHECK_INFO_LOG(glCheckProgram, glGetProgramiv, glGetProgramInfoLog, GL_LINK_STATUS,    "link program")
 #undef GL_CHECK_INFO_LOG
+
+
+// OpenGL Extension Setup
+// -----------------------------------------------------------------------------
+
+static struct {
+  usize ARB_ES2_compatibility:                1;
+  usize ARB_clear_buffer_object:              1;
+  usize ARB_color_buffer_float:               1;
+  usize ARB_compressed_texture_pixel_storage: 1;
+  usize ARB_copy_buffer:                      1;
+  usize ARB_debug_output:                     1;
+  usize ARB_depth_texture:                    1;
+  usize ARB_draw_elements_base_vertex:        1;
+  usize ARB_explicit_attrib_location:         1;
+  usize ARB_explicit_uniform_location:        1;
+  usize ARB_fragment_coord_conventions:       1;
+  usize ARB_framebuffer_object:               1;
+  usize ARB_framebuffer_sRGB:                 1;
+  usize ARB_get_program_binary:               1;
+  usize ARB_get_texture_sub_image:            1;
+  usize ARB_half_float_pixel:                 1;
+  usize ARB_half_float_vertex:                1;
+  usize ARB_internalformat_query:             1;
+  usize ARB_internalformat_query2:            1;
+  usize ARB_invalidate_subdata:               1;
+  usize ARB_map_buffer_alignment:             1;
+  usize ARB_map_buffer_range:                 1;
+  usize ARB_multi_bind:                       1;
+  usize ARB_multisample:                      1;
+  usize ARB_occlusion_query2:                 1;
+  usize ARB_program_interface_query:          1;
+  usize ARB_provoking_vertex:                 1;
+  usize ARB_robustness:                       1;
+  usize ARB_sampler_objects:                  1;
+  usize ARB_separate_shader_objects:          1;
+  usize ARB_shader_texture_lod:               1;
+  usize ARB_sync:                             1;
+  usize ARB_texture_filter_anisotropic:       1;
+  usize ARB_texture_float:                    1;
+  usize ARB_texture_rectangle:                1;
+  usize ARB_texture_storage:                  1;
+  usize ARB_texture_swizzle:                  1;
+  usize ARB_vertex_array_bgra:                1;
+  usize ARB_vertex_array_object:              1;
+  usize ARB_vertex_attrib_binding:            1;
+  usize ARB_window_pos:                       1;
+  usize EXT_abgr:                             1;
+  usize EXT_framebuffer_blit:                 1;
+  usize EXT_framebuffer_object:               1;
+  usize EXT_framebuffer_sRGB:                 1;
+  usize EXT_packed_depth_stencil:             1;
+  usize EXT_provoking_vertex:                 1;
+  usize EXT_texture_compression_dxt1:         1;
+  usize EXT_texture_compression_s3tc:         1;
+  usize EXT_texture_filter_anisotropic:       1;
+  usize EXT_texture_rectangle:                1;
+  usize EXT_texture_sRGB_decode:              1;
+  usize EXT_texture_swizzle:                  1;
+  usize EXT_vertex_array_bgra:                1;
+  usize KHR_debug:                            1;
+  usize KHR_context_flush_control:            1;
+  usize KHR_no_error:                         1;
+  usize OES_EGL_image:                        1;
+  usize OES_read_format:                      1;
+  usize ANGLE_texture_compression_dxt3:       1;
+  usize ANGLE_texture_compression_dxt5:       1;
+  usize AMD_shader_trinary_minmax:            1;
+  usize ATI_texture_float:                    1;
+  usize MESA_window_pos:                      1;
+  usize NV_packed_depth_stencil:              1;
+  usize NV_primitive_restart:                 1;
+  usize NV_texture_rectangle:                 1;
+  usize S3_s3tc:                              1;
+  usize SGIS_generate_mipmap:                 1;
+} exts;
+
+static void setupExtension(std::string_view ext) {
+#if BUILD_DEVELOPMENT
+# define IGNORE(name) else if ("GL_"#name##sv == ext) {}
+#else
+# define IGNORE(name)
+#endif
+#define TEST(name, body) else if ("GL_"#name##sv == ext) {  \
+    exts.name = true; body                                  \
+  }
+
+  if (ext.empty()) {} // The GL_EXTENSIONS string can end with a space.
+  TEST(ARB_ES2_compatibility, { // 4.1
+  })
+  TEST(ARB_clear_buffer_object, { // 4.3
+  })
+  TEST(ARB_color_buffer_float, {
+  })
+  TEST(ARB_compressed_texture_pixel_storage, { // 4.2
+  })
+  TEST(ARB_copy_buffer, { // 3.0
+  })
+  TEST(ARB_debug_output, { // 4.3
+  })
+  TEST(ARB_draw_elements_base_vertex, { // 3.2
+  })
+  TEST(ARB_explicit_attrib_location, { // 3.3
+  })
+  TEST(ARB_explicit_uniform_location, { // 4.3
+  })
+  TEST(ARB_fragment_coord_conventions, {
+  })
+  TEST(ARB_framebuffer_object, { // 3.0
+  })
+  TEST(ARB_framebuffer_sRGB, { // 3.0
+  })
+  TEST(ARB_get_program_binary, { // 4.1
+  })
+  TEST(ARB_get_texture_sub_image, { // 4.5
+  })
+  TEST(ARB_half_float_pixel, { // 3.1
+  })
+  TEST(ARB_half_float_vertex, { // 3.1
+  })
+  TEST(ARB_internalformat_query, { // 4.2
+  })
+  TEST(ARB_internalformat_query2, { // 4.3
+  })
+  TEST(ARB_invalidate_subdata, { // 4.3
+  })
+  TEST(ARB_map_buffer_alignment, { // 4.2
+  })
+  TEST(ARB_map_buffer_range, { // 3.0
+  })
+  TEST(ARB_multi_bind, { // 4.4
+  })
+  TEST(ARB_multisample, { // 3.0
+  })
+  TEST(ARB_occlusion_query2, { // 3.3
+  })
+  TEST(ARB_program_interface_query, { // 4.3
+  })
+  TEST(ARB_provoking_vertex, { // 3.2
+  })
+  TEST(ARB_robustness, { // 4.5
+  })
+  TEST(ARB_sampler_objects, { // 3.3
+  })
+  TEST(ARB_separate_shader_objects, { // 4.1
+  })
+  TEST(ARB_shader_texture_lod, {
+  })
+  TEST(ARB_sync, { // 3.2
+  })
+  TEST(ARB_texture_filter_anisotropic, {
+  })
+  TEST(ARB_texture_float, { // 3.0
+  })
+  TEST(ARB_texture_storage, { // 4.2
+  })
+  TEST(ARB_texture_swizzle, { // 3.3
+  })
+  TEST(ARB_texture_rectangle, { // 3.1
+  })
+  TEST(ARB_vertex_array_object, { // 3.0
+  })
+  TEST(ARB_vertex_attrib_binding, { // 4.3
+  })
+  TEST(ARB_vertex_array_bgra, { // 3.2
+  })
+  TEST(ARB_window_pos, {
+  })
+  TEST(EXT_abgr, {})
+  TEST(EXT_framebuffer_blit, { // 3.0
+
+  })
+  TEST(EXT_framebuffer_object, { // 3.0
+
+  })
+  TEST(EXT_framebuffer_sRGB, { // 3.0
+
+  })
+  TEST(EXT_packed_depth_stencil, { // 3.0
+
+  })
+  TEST(EXT_provoking_vertex, { // 3.2
+
+  })
+  TEST(EXT_texture_compression_dxt1, {})
+  TEST(EXT_texture_compression_s3tc, {})
+  TEST(EXT_texture_filter_anisotropic, {})
+  TEST(EXT_texture_rectangle, { // 3.1
+
+  })
+  TEST(EXT_texture_sRGB_decode, {
+
+  })
+  TEST(EXT_texture_swizzle, { // 3.3
+
+  })
+  TEST(EXT_vertex_array_bgra, { // 3.2
+
+  })
+  TEST(KHR_context_flush_control, {
+  })
+  TEST(KHR_debug, {
+  })
+  TEST(KHR_no_error, {
+  })
+  TEST(OES_EGL_image, {
+
+  })
+  TEST(OES_read_format, { // 4.1
+
+  })
+  TEST(ANGLE_texture_compression_dxt3, {})
+  TEST(ANGLE_texture_compression_dxt5, {})
+  TEST(AMD_shader_trinary_minmax, {})
+  TEST(ATI_texture_float, {}) // 3.0
+  TEST(MESA_window_pos, {
+  })
+  TEST(NV_packed_depth_stencil, {}) // 3.0
+  TEST(NV_primitive_restart, {
+  })
+  TEST(NV_texture_rectangle, { // 3.1
+  })
+  TEST(S3_s3tc, {})
+  TEST(SGIS_generate_mipmap, { // 3.0
+  })
+  IGNORE(ARB_multitexture)             // 1.2
+  IGNORE(ARB_texture_border_clamp)     // 1.3
+  IGNORE(ARB_texture_compression)      // 1.3
+  IGNORE(ARB_texture_cube_map)         // 1.3
+  IGNORE(ARB_depth_texture)            // 1.4
+  IGNORE(ARB_point_parameters)         // 1.4
+  IGNORE(ARB_texture_mirrored_repeat)  // 1.4
+  IGNORE(ARB_occlusion_query)          // 1.5
+  IGNORE(ARB_vertex_buffer_object)     // 1.5
+  IGNORE(ARB_draw_buffers)             // 2.0
+  IGNORE(ARB_fragment_shader)          // 2.0
+  IGNORE(ARB_shader_objects)           // 2.0
+  IGNORE(ARB_shading_language_100)     // 2.0
+  IGNORE(ARB_texture_non_power_of_two) // 2.0
+  IGNORE(ARB_vertex_shader)            // 2.0
+  IGNORE(ARB_pixel_buffer_object)      // 2.1
+  IGNORE(EXT_copy_texture)             // 1.1
+  IGNORE(EXT_subtexture)               // 1.1
+  IGNORE(EXT_texture)                  // 1.1
+  IGNORE(EXT_texture_object)           // 1.1
+  IGNORE(EXT_vertex_array)             // 1.1
+  IGNORE(EXT_bgra)                     // 1.2
+  IGNORE(EXT_draw_range_elements)      // 1.2
+  IGNORE(EXT_packed_pixels)            // 1.2
+  IGNORE(EXT_texture3D)                // 1.2
+  IGNORE(EXT_texture_edge_clamp)       // 1.2
+  IGNORE(EXT_texture_cube_map)         // 1.3
+  IGNORE(EXT_blend_color)              // 1.3
+  IGNORE(EXT_blend_func_separate)      // 1.4
+  IGNORE(EXT_blend_minmax)             // 1.4
+  IGNORE(EXT_blend_subtract)           // 1.4
+  IGNORE(EXT_multi_draw_arrays)        // 1.4
+  IGNORE(EXT_point_parameters)         // 1.4
+  IGNORE(EXT_stencil_wrap)             // 1.4
+  IGNORE(EXT_texture_lod_bias)         // 1.4
+  IGNORE(EXT_blend_equation_separate)  // 2.0
+  IGNORE(EXT_pixel_buffer_object)      // 2.1
+  IGNORE(EXT_texture_sRGB)             // 2.1
+  IGNORE(APPLE_packed_pixels)          // 1.2
+  IGNORE(ATI_blend_equation_separate)  // 2.0
+  IGNORE(ATI_separate_stencil)         // 2.0
+  IGNORE(IBM_texture_mirrored_repeat)  // 1.4
+  IGNORE(INGR_blend_func_separate)     // 1.4
+  IGNORE(SGIS_texture_edge_clamp)      // 1.2
+  IGNORE(SGIS_texture_lod)             // 1.2
+  IGNORE(SGIS_texture_border_clamp)    // 1.3
+  IGNORE(SUN_multi_draw_arrays)        // 1.4
+  // Deprecated / Misc. Ignored
+  IGNORE(ARB_fragment_program)
+  IGNORE(ARB_fragment_program_shadow)
+  IGNORE(ARB_point_sprite)
+  IGNORE(ARB_shadow)
+  IGNORE(ARB_texture_env_add)
+  IGNORE(ARB_texture_env_combine)
+  IGNORE(ARB_texture_env_crossbar)
+  IGNORE(ARB_texture_env_dot3)
+  IGNORE(ARB_transpose_matrix)
+  IGNORE(ARB_vertex_program)
+  IGNORE(EXT_compiled_vertex_array)
+  IGNORE(EXT_fog_coord)
+  IGNORE(EXT_gpu_program_parameters)
+  IGNORE(EXT_rescale_normal)
+  IGNORE(EXT_texture_env_add)
+  IGNORE(EXT_texture_env_combine)
+  IGNORE(EXT_texture_env_dot3)
+  IGNORE(EXT_texture_shadow_funcs)
+  IGNORE(EXT_secondary_color)
+  IGNORE(EXT_separate_specular_color)
+  IGNORE(EXT_shadow_funcs)
+  IGNORE(EXT_stencil_two_side)
+  IGNORE(ATI_draw_buffers)
+  IGNORE(ATI_texture_env_combine3)
+  IGNORE(ATI_fragment_shader)
+  IGNORE(IBM_multimode_draw_arrays)
+  IGNORE(IBM_rasterpos_clip)
+  IGNORE(MESA_pack_invert)
+  IGNORE(NV_blend_square)
+  IGNORE(NV_fog_distance)
+  IGNORE(NV_light_max_exponent)
+  IGNORE(NV_texgen_reflection)
+  IGNORE(NV_texture_env_combine4)
+  else {
+    LOG(App, Warn, "Unknown GL Extension: %.*s", static_cast<u32>(ext.size()), ext.data());
+  }
+
+#undef TEST
+#undef IGNORE
+}
+
 
 // OpenGL + ImGui test
 // -----------------------------------------------------------------------------
@@ -245,8 +579,56 @@ void* renderMain(void* arg) {
     LOG(App, Info, "OpenGL Vendor: %s", glGetString(GL_VENDOR));
     LOG(App, Info, "OpenGL Renderer: %s", glGetString(GL_RENDERER));
 
-    i32 numExts;
-    glGetIntegerv(GL_NUM_EXTENSIONS, &numExts);
+    u16 glVersion;
+    {
+      i32 major, minor;
+      glGetIntegerv(GL_MAJOR_VERSION, &major);
+      glGetIntegerv(GL_MINOR_VERSION, &minor);
+
+      switch (glGetError()) {
+      default:
+        glCheck();
+        break;
+
+      case GL_NO_ERROR:
+        glVersion = glMakeVersion(major, minor);
+        break;
+
+      case GL_INVALID_ENUM:
+        auto str{ reinterpret_cast<char const*>(glGetString(GL_VERSION)) };
+        auto len{ strlen(str) };
+
+        // NOTE: assuming "x.y" format: no versions are double digits
+        if (len < 3 || str[1] != '.' || !isdigit(str[0]) || !isdigit(str[2])) {
+          abort(); // TODO fatal error handling
+        }
+        glVersion = glMakeVersion(str[0] - '0', str[2] - '0');
+        break;
+      }
+    }
+
+    if (glVersion < GL3_0) {
+      auto str{ reinterpret_cast<char const*>(glGetString(GL_EXTENSIONS)) };
+      for (;;) {
+        auto end{ strchr(str, ' ') };
+        if (!end) {
+          setupExtension({ str });
+          break;
+        }
+
+        setupExtension({ str, end - str });
+        str = end + 1;
+      }
+    }
+    else {
+      i32 numExts;
+      glGetIntegerv(GL_NUM_EXTENSIONS, &numExts);
+
+      for (auto i{0}; i < numExts; i++) {
+        auto str{ reinterpret_cast<char const*>(glGetStringi(GL_EXTENSIONS, i)) };
+        setupExtension({ str });
+      }
+    }
 
 #if PLATFORM_LINUX || PLATFORM_WINDOWS
 #define GL(type, name) gl##name = reinterpret_cast<PFNGL##type##PROC>(gl->getProcAddress("gl" #name))
--- engine/app/linux/LinuxMain.cpp
+++ engine/app/linux/LinuxMain.cpp
@@ -1,5 +1,121 @@
-#include "core/Core.hpp"
+#include "app/App.hpp"
+#include "app/GfxTest.hpp"
 
-i32 main(i32 argc, char* argv[]) {
+#include <GL/glx.h>
+#include <X11/Xlib-xcb.h>
+
+#include <stdlib.h>
+
+class LinuxOpenGL : public OpenGL {
+public:
+  Display*    display;
+  GLXDrawable drawable;
+  GLXContext  context;
+
+  void* getProcAddress(char const* name) override {
+    return reinterpret_cast<void*>(glXGetProcAddress(reinterpret_cast<GLubyte const*>(name)));
+  }
+
+  void clearCurrent() override {
+    auto result{ glXMakeContextCurrent(display, 0, 0, None) };
+    ASSERT(result);
+  }
+
+  void makeCurrent() override {
+    auto result{ glXMakeContextCurrent(display, drawable, drawable, context) };
+    ASSERT(result);
+  }
+
+  f64 getDeltaTime() override {
+    return .016;
+  }
+};
+
+static i32 visualAttrs[]{
+  GLX_X_RENDERABLE, True,
+  GLX_DRAWABLE_TYPE, GLX_WINDOW_BIT,
+  GLX_RENDER_TYPE, GLX_RGBA_BIT,
+  GLX_X_VISUAL_TYPE, GLX_TRUE_COLOR,
+  GLX_RED_SIZE, 8,
+  GLX_GREEN_SIZE, 8,
+  GLX_BLUE_SIZE, 8,
+  GLX_ALPHA_SIZE, 0,
+  GLX_DEPTH_SIZE, 0,
+  GLX_STENCIL_SIZE, 0,
+  GLX_DOUBLEBUFFER, True,
+  None
+};
+
+i32 main(i32 argc UNUSED, char* argv UNUSED[]) {
+  LinuxOpenGL gl;
+  gl.width = 800;
+  gl.height = 600;
+  gl.display = XOpenDisplay(nullptr);
+  ASSERT(gl.display);
+
+  auto conn{ XGetXCBConnection(gl.display) };
+  ASSERT(conn);
+
+  XSetEventQueueOwner(gl.display, XCBOwnsEventQueue);
+
+  auto defaultScreen{ DefaultScreen(gl.display) };
+
+  xcb_screen_t* screen;
+  {
+    auto iter{ xcb_setup_roots_iterator(xcb_get_setup(conn)) };
+    for (auto n{ defaultScreen }; iter.rem && n > 0; xcb_screen_next(&iter), n--);
+    screen = iter.data;
+  }
+
+  i32 numConfigs;
+  auto configs{ glXChooseFBConfig(gl.display, defaultScreen, visualAttrs, &numConfigs) };
+  ASSERT(configs && numConfigs);
+
+  i32 visualId;
+  glXGetFBConfigAttrib(gl.display, configs[0], GLX_VISUAL_ID, &visualId);
+
+  gl.context = glXCreateNewContext(gl.display, configs[0], GLX_RGBA_TYPE, 0, True);
+  ASSERT(gl.context);
+
+  auto colormap{ xcb_generate_id(conn) };
+  auto window  { xcb_generate_id(conn) };
+
+  xcb_create_colormap(conn, XCB_COLORMAP_ALLOC_NONE, colormap, screen->root, visualId);
+
+  auto valueMask{ XCB_CW_EVENT_MASK | XCB_CW_COLORMAP };
+  u32 valueList[]{
+    XCB_EVENT_MASK_EXPOSURE | XCB_EVENT_MASK_KEY_PRESS,
+    colormap,
+    0
+  };
+  xcb_create_window(conn, XCB_COPY_FROM_PARENT, window, screen->root, 0, 0, gl.width, gl.height,
+                    0, XCB_WINDOW_CLASS_INPUT_OUTPUT, visualId, valueMask, valueList);
+
+  xcb_map_window(conn, window);
+
+  gl.drawable = glXCreateWindow(gl.display, configs[0], window, 0);
+  ASSERT(gl.drawable);
+
+  while (true) {
+    auto event{ xcb_wait_for_event(conn) };
+    ASSERT(event);
+
+    switch (event->response_type & ~0x80) {
+    case XCB_KEY_PRESS:
+      break;
+
+    case XCB_EXPOSE:
+      renderMain(&gl);
+      break;
+    }
+
+    free(event);
+  }
+
+  glXDestroyWindow(gl.display, gl.drawable);
+  xcb_destroy_window(conn, window);
+  glXDestroyContext(gl.display, gl.context);
+
+  XCloseDisplay(gl.display);
   return 0;
 }
